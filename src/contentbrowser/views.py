1d89e8ef2a20ab3c24edc45bcc6d51d865aabd6f
Andre Engelbrecht <andre@teh-node.co.za>
2011-11-12 18:41:25 +0000

    We should be able to specify restrictions to which groups are allowed to access the browser items

--- src/contentbrowser/views.py
+++ src/contentbrowser/views.py
@@ -1,16 +1,36 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django import http
+from django.conf import settings
 
 from core import ContentBrowser
 
-class BrowserItemsView(TemplateView):
-	template_name = 'contentbrowser/browser_items.html'
 
-	def get_context_data(self, **kwargs):
-		c = super(BrowserItemsView, self).get_context_data(**kwargs)
-		ctype = self.request.GET.get('ctype', None)
-		cb = ContentBrowser()
-		if ctype:
-			c['%s_items' % ctype.replace('.', '_')] = cb.get_items_for(ctype)
-		else:
-			c['empty_items'] = True
-		return c
+class BrowserItemsView(TemplateView):
+    template_name = 'contentbrowser/browser_items.html'
+
+    @method_decorator(login_required)
+    def get(self, *args, **kwargs):
+        user_groups = self.request.user.groups.values_list('name', flat=True)
+        CONTENT_BROWSER_RESTRICTED_TO = getattr(
+            settings, 'CONTENT_BROWSER_RESTRICTED_TO', None)
+
+        if not CONTENT_BROWSER_RESTRICTED_TO:
+            return super(BrowserItemsView, self).get(*args, **kwargs)
+
+        for group in user_groups:
+            if group in CONTENT_BROWSER_RESTRICTED_TO:
+                return super(BrowserItemsView, self).get(*args, **kwargs)
+
+        return http.HttpResponseForbidden()
+
+    def get_context_data(self, **kwargs):
+        c = super(BrowserItemsView, self).get_context_data(**kwargs)
+        ctype = self.request.GET.get('ctype', None)
+        cb = ContentBrowser()
+        if ctype:
+            c['%s_items' % ctype.replace('.', '_')] = cb.get_items_for(ctype)
+        else:
+            c['empty_items'] = True
+        return c
