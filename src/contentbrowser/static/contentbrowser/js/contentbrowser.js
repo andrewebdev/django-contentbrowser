95c75357b486e77cdfa440d99b7a3774ddc21b5e
Andre Engelbrecht <andre@teh-node.co.za>
2011-11-14 22:34:43 +0000

    Still optimizing the contentbrowser.

---
+++ src/contentbrowser/static/contentbrowser/js/contentbrowser.js
@@ -0,0 +1,56 @@
+
+function ContentBrowser(options) {
+    var self = this,
+        defaultOptions = {
+            cb_actions: {}
+        };
+
+    /** Extend the default options **/
+    if (typeof options === 'object') {
+        options = $.extend(defaultOptions, options);
+    } else {
+        options = defaultOptions;
+    }
+
+    /** Private parts **/
+
+    /** Public parts **/
+    this.showLoader = function(container) {
+        var loader_path = options.static_prefix + 'contentbrowser/img/ajax-loader.gif';
+        django.jQuery(container).empty()
+            .html('<div style="text-align:center;"><img src="' + loader_path + '" /></div>');
+    }
+
+    this.takeAction = function(action, el) {
+        return options.cb_actions[action](django.jQuery(el));
+    };
+    
+    this.loadItems = function(url) {
+        self.showLoader('#cb_items_panel');
+        django.jQuery.ajax({
+            url: url,
+            success: function(data) {
+                django.jQuery('#cb_items_panel').empty().html(data);
+            }
+        });
+        return false;
+    };
+
+}
+
+
+django.jQuery(document).ready(function() {
+
+    django.jQuery('#cb_toggle_panels').click(function() {
+        django.jQuery('#cb_types_panel, #cb_items_panel, #cb_display_panel, #cb_filter_panel')
+            .toggle(300);
+        return false;
+    });
+
+    django.jQuery('#cb_types_panel a').click(function() {
+        django.jQuery('#cb_types_panel li a').removeClass('selected');
+        django.jQuery(this).addClass('selected');
+        return cb.loadItems(django.jQuery(this).attr('href'));
+    });
+
+});
