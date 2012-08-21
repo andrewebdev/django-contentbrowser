d20bafeaea1f02f6c373488fc04fda6b019ca113
Andre Engelbrecht <andre@teh-node.co.za>
2012-08-21 15:49:12 +0100

    Changed the package layout

--- setup.py
+++ setup.py
@@ -1,15 +1,36 @@
-from setuptools import setup, find_packages
+import os
+from disutils.core import setup
+from disutils.command.install import INSTALL_SCHEMES
+
+
+version = '.'.join([str(i) for i in __import__('contentbrowser').VERSION])
+
 
 setup(
     name="contentbrowser",
-    version="0.1",
+    version=version,
     url="",
-    description="An app to manage content in any django based cms.",
+    description="An app to browse through a library of content in a django based cms.",
     author="Andre Engelbrecht",
+    author_email='andre@teh-node.co.za',
+    license='MIT',
+    download_url='https://github.com/andrewebdev/django-contentbrowser/tarball/master',
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    keywords=['django', 'content', 'browser', 'library'],
     install_requires=[
         'setuptools',
-        'django-appregister == 0.2',
+        'django-appregister == 0.3.1',
     ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    long_description="""\
+"""
 )
